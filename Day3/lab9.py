782b3f2735043492f4dc86b6c61f2843257252d0
joshiroshan052 <joshiroshan052@gmail.com>
2021-12-06 13:29:16 +0545

    Lab4

--- Day3/lab9.py
+++ Day3/lab9.py
@@ -1,20 +1,7 @@
 #Take username and password from user and check it again for the three times whether
 # the user has entered the rightusername and password.
-# If yes then print a message "Logged in Successfully", if not then print invalid credentials
-#for consecutive 3 times and if the limit exceeds than print "Attempt finished".
-
-user_name = str(input("enter the username"))
-pass_word = str(input("enter the password"))
-
-for i in range(0, 3):
-    x = str(input("enter your username"))
-    y = str(input("enter your password"))
-    if x == user_name and y == pass_word:
-        print("login successful")
-        break
-    elif i < 3:
-        if x != user_name and y != pass_word and i < 3:
-            print("invalid credentials")
+n = int(input("enter a number: "))
+if (n=='A' or n=='E' or n=='O'):
+    print("the number is")
 else:
-    print("exceeded 3 attempts")
-
+    print("nii")
